c295a5ea07ccd170e3dd7462c592fb1f2e799e48
Aleph Cadena <alephcadena@gmail.com>
2021-11-28 18:44:08 -0600

    api route

---
+++ routes/api.js
@@ -0,0 +1,58 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.get("/workouts", (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: { totalDuration: { $sum: "$exercises.duration" } },
+    },
+  ])
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.put("/workouts/:id", (req, res) => {
+  Workout.findByIdAndUpdate(
+    { _id: req.params.id },
+    { $push: { exercises: req.body } }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.post("/workouts", (req, res) => {
+  Workout.create(req)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/workouts/range", (req, res) => {
+
+    Workout.aggregate([
+        {
+        $addFields: { totalDuration: { $sum: "$exercises.duration" } },
+        },
+    ])
+
+    .sort({ day: -1 })
+    .limit(7).sort({day:1})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+module.exports = router;
